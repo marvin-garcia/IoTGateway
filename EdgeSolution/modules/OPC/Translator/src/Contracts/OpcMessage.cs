e4f2aa5d8a75ca8535e79a54b48262e2a9a63441
Ubuntu <magar@IoTDev.runybhcwkene1asd0nxq4ruovd.jx.internal.cloudapp.net>
2020-07-20 15:44:35 +0000

    added default value to status property in opc message

--- EdgeSolution/modules/OPC/Translator/src/Contracts/OpcMessage.cs
+++ EdgeSolution/modules/OPC/Translator/src/Contracts/OpcMessage.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace MicrosoftSolutions.IoT.Edge.OpcToDtdl.Contracts
 {
@@ -9,6 +11,8 @@ namespace MicrosoftSolutions.IoT.Edge.OpcToDtdl.Contracts
         public string NodeId  { get; set; }
         public string ApplicationUri { get; set; }
         public string DisplayName { get; set; }
+        [DefaultValue("OK")]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
         public string Status { get; set; }
         public OpcMessageValue Value { get; set; }
     }
