abed18e703e7ee743acba268ec869065e13ccbcb
Ubuntu <magar@IoTDev.runybhcwkene1asd0nxq4ruovd.jx.internal.cloudapp.net>
2020-10-30 13:48:05 +0000

    added templates and scripts to configure opc nodes in IoT edeg vm

---
+++ Scripts/publishNodes.ps1
@@ -0,0 +1,27 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$serverFqdn,
+    [Parameter(Mandatory)]
+    [string]$serverPorts,
+    [Parameter(Mandatory)]
+    [string]$opcNodesPath,
+    [Parameter(Mandatory)]
+    [string]$publishedNodesPath
+)
+
+Set-Content -Path ./logs.txt -Value "ServerFQDN: $($serverFqdn), ServerPorts: $($serverPorts). OpcNodesPath: $($opcNodesPath). PublishNodesPath: $($publishedNodesPath)"
+
+[Array]$serverPortList = $serverPorts.Split(',')
+$opcNodes = ConvertFrom-Json -InputObject (Get-Content -Path $opcNodesPath -Raw)
+$publishedNodes = @()
+
+foreach ($port in $serverPortList)
+{
+    $publishedNodes += @{
+        "EndpointUrl" = "opc.tcp://$($serverFqdn):$($port)/"
+        "UseSecurity" = $false
+        "OpcNodes" = $opcNodes.OpcNodes
+    }
+}
+
+Set-Content -Path $publishedNodesPath -Value (ConvertTo-Json -InputObject $publishedNodes -Depth 5)
