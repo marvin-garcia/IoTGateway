5fa3dad14faadcddbf4f94a25394a5465a07cad7
Ubuntu <magar@IoTDev.runybhcwkene1asd0nxq4ruovd.jx.internal.cloudapp.net>
2020-11-04 16:11:21 +0000

    collect parameters in function flow rather than function declaration

--- Scripts/deploy.ps1
+++ Scripts/deploy.ps1
@@ -1,27 +1,84 @@
 ## https://docs.microsoft.com/en-us/azure/stream-analytics/stream-analytics-cicd-api
 
 function New-IIoTEnvironment(
-    [string]$location = "eastus",
-    [string]$resource_group,
-    [string]$iot_hub_sku = "S1",
-    [string]$simulation_vm_size = "Standard_D2s_v3",
-    [string]$edge_vm_size = "Standard_D2s_v3",
-    [int]$eventhubs_message_retention = 7,
-    [string]$notifications_webhook,
-    [string]$alerts_webhook,
+    [string]$webhook_url,
     [bool]$deploy_time_series_insights = $true
 )
 {
-    if (!$notifications_webhook)
+    if (!$webhook_url)
     {
-        Write-Error "You need to provide a notification webhook. If you don't have one yet, you can get one for free at https://webhook.site/"
+        Write-Error "You need to provide a webhook for notifications and alerts. If you don't have one yet, you can get one for free at https://webhook.site/"
         return $null
     }
-    if (!$notifications_webhook)
+
+    #region obtain deployment location
+    Write-Host "Please choose a location for your deployment from this list (using its Index):"
+    $script:index = 0
+    $locations | Format-Table -AutoSize -property `
+    @{Name = "Index"; Expression = { ($script:index++) } }, `
+    @{Name = "Location"; Expression = { $_.DisplayName } } `
+    | Out-Host
+    while ($true)
     {
-        Write-Error "You need to provide an alert webhook. If you don't have one yet, you can get one for free at https://webhook.site/"
-        return $null
+        $option = Read-Host -Prompt ">"
+        try
+        {
+            if ([int]$option -ge 1 -and [int]$option -le $locations.Count)
+            {
+                break
+            }
+        }
+        catch
+        {
+            Write-Host "Invalid index '$($option)' provided."
+        }
+        Write-Host "Choose from the list using an index between 1 and $($locations.Count)."
     }
+    $location = $locations[$option - 1].Location
+    #endregion
+
+    #region obtain resource group name
+    $first = $true
+    while ([string]::IsNullOrEmpty($script:resourceGroupName) `
+            -or ($script:resourceGroupName -notmatch "^[a-z0-9-_]*$"))
+    {
+        if (!$script:interactive)
+        {
+            throw "Invalid resource group name specified which is mandatory for non-interactive script use."
+        }
+        if ($first -eq $false)
+        {
+            Write-Host "Use alphanumeric characters as well as '-' or '_'."
+        }
+        else
+        {
+            Write-Host
+            Write-Host "Please provide a name for the resource group."
+            $first = $false
+        }
+        $script:resourceGroupName = Read-Host -Prompt ">"
+    }
+
+    $resourceGroup = Get-AzResourceGroup -Name $script:resourceGroupName `
+        -ErrorAction SilentlyContinue
+    if (!$resourceGroup)
+    {
+        Write-Host "Resource group '$script:resourceGroupName' does not exist."
+        Select-ResourceGroupLocation
+        $resourceGroup = New-AzResourceGroup -Name $script:resourceGroupName `
+            -Location $script:resourceGroupLocation
+        Write-Host "Created new resource group $($script:resourceGroupName) in $($resourceGroup.Location)."
+        Set-ResourceGroupTags -state "Created"
+        return $True
+    }
+    else
+    {
+        Set-ResourceGroupTags -state "Updating"
+        $script:resourceGroupLocation = $resourceGroup.Location
+        Write-Host "Using existing resource group $($script:resourceGroupName)..."
+        return $False
+    }
+    #endregion
 
     $env_hash = Get-EnvironmentHash -resource_group $resource_group
     $iot_hub_name = "iothub-$($env_hash)"
@@ -42,10 +99,45 @@ function New-IIoTEnvironment(
     # OPC Sim VM parameters
     $sim_vm_name = "opc-sim"
     # $sim_vm_dns = "$($sim_vm_name)-$($env_hash)"
-    
+
+    # We will use VM with at least 1 core and 2 GB of memory for hosting OPC PLC simulatoin containers.
+    $simulation_vm_sizes = Get-AzVMSize $script:resourceGroupLocation `
+    | Where-Object { $availableVmNames -icontains $_.Name } `
+    | Where-Object {
+        ($_.NumberOfCores -ge 1) -and `
+        ($_.MemoryInMB -ge 2048) -and `
+        ($_.OSDiskSizeInMB -ge 1047552) -and `
+        ($_.ResourceDiskSizeInMB -ge 4096)
+    } `
+    | Sort-Object -Property `
+        NumberOfCores, MemoryInMB, ResourceDiskSizeInMB, Name
+    # Pick top
+    if ($simulation_vm_sizes.Count -ne 0)
+    {
+        $simulation_vm_size = $simulation_vm_sizes[0].Name
+        Write-Host "Using $($simulation_vm_size) as VM size for all edge simulation hosts..."
+    }
+
     # IoT Edge VM parameters
     $edge_vm_name = "linuxgateway-1"
     $published_nodes_path = "/appdata/publishednodes.json"
+    
+    # We will use VM with at least 2 cores and 8 GB of memory as gateway host.
+    $edge_vm_sizes = Get-AzVMSize $script:resourceGroupLocation `
+        | Where-Object { $availableVmNames -icontains $_.Name } `
+        | Where-Object {
+            ($_.NumberOfCores -ge 2) -and `
+            ($_.MemoryInMB -ge 8192) -and `
+            ($_.OSDiskSizeInMB -ge 1047552) -and `
+            ($_.ResourceDiskSizeInMB -gt 8192)
+        } `
+        | Sort-Object -Property `
+            NumberOfCores,MemoryInMB,ResourceDiskSizeInMB,Name
+    # Pick top
+    if ($edge_vm_sizes.Count -ne 0) {
+        $edge_vm_size = $edge_vm_sizes[0].Name
+        Write-Host "Using $($edge_vm_size) as VM size for all edge simulation gateway hosts..."
+    }
 
     # virtual network parameters
     # $vnet_name = "iiot-$($env_hash)-vnet"
@@ -60,6 +152,7 @@ function New-IIoTEnvironment(
     # $persistent_storage_container = "telemetry"
 
     # event hubs
+    $eventhubs_message_retention = 7
     # $eh_name = "eh-$($env_hash)"
     # $eh_notifications_name = "notifications"
     # $eh_notifications_la_consumer_group = "logicapp"
@@ -142,8 +235,8 @@ function New-IIoTEnvironment(
         "adxAccessPolicyPrincipalType" = @{ "value" = $adx_principal_type }
         "adxAccessPolicyRole" = @{ "value" = $adx_access_role }
         "adxAccessPrincipalAssignmentId" = @{ "value" = $userid }
-        "notificationsWebhookUrl" = @{ "value" = $notifications_webhook }
-        "alertsWebhookUrl" = @{ "value" = $alerts_webhook }
+        "notificationsWebhookUrl" = @{ "value" = $webhook_url }
+        "alertsWebhookUrl" = @{ "value" = $webhook_url }
         "edgeASAJobName" = @{ "value" = $edge_asa_name }
         "edgeASAJobQuery" = @{ "value" = ($edge_asa_query | Out-String) }
         "edgeASAJobInputName" = @{ "value" = $edge_asa_input_name }
